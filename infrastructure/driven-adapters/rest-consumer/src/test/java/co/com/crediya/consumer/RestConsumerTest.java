792bbe0bc8a631da5549bf4f15abc82e2cf88bf2
Georffrey <georffreyarevalo@gmail.com>
2025-08-25 16:22:50 -0500

    Test in rest consumer adapter

--- infrastructure/driven-adapters/rest-consumer/src/test/java/co/com/crediya/consumer/RestConsumerTest.java
+++ infrastructure/driven-adapters/rest-consumer/src/test/java/co/com/crediya/consumer/RestConsumerTest.java
@@ -1,6 +1,7 @@
 package co.com.crediya.consumer;
 
 
+import co.com.crediya.consumer.mappers.UserConsumerMapper;
 import co.com.crediya.consumer.rest.UserRestConsumer;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,6 +18,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 import java.io.IOException;
 
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 class RestConsumerTest {
 
@@ -22,48 +27,38 @@ class RestConsumerTest {
 
     private static MockWebServer mockBackEnd;
 
+    @Mock
+    private static UserConsumerMapper userConsumerMapper;
 
-//    @BeforeAll
-//    static void setUp() throws IOException {
-//        mockBackEnd = new MockWebServer();
-//        mockBackEnd.start();
-//        var webClient = WebClient.builder().baseUrl(mockBackEnd.url("/").toString()).build();
-//        restConsumer = new UserRestConsumer(webClient);
-//    }
-//
-//    @AfterAll
-//    static void tearDown() throws IOException {
-//
-//        mockBackEnd.shutdown();
-//    }
-//
-//    @Test
-//    @DisplayName("Validate the function testGet.")
-//    void validateTestGet() {
-//
-//        mockBackEnd.enqueue(new MockResponse()
-//                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .setResponseCode(HttpStatus.OK.value())
-//                .setBody("{\"state\" : \"ok\"}"));
-//        var response = restConsumer.testGet();
-//
-//        StepVerifier.create(response)
-//                .expectNextMatches(objectResponse -> objectResponse.getState().equals("ok"))
-//                .verifyComplete();
-//    }
-//
-//    @Test
-//    @DisplayName("Validate the function testPost.")
-//    void validateTestPost() {
-//
-//        mockBackEnd.enqueue(new MockResponse()
-//                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .setResponseCode(HttpStatus.OK.value())
-//                .setBody("{\"state\" : \"ok\"}"));
-//        var response = restConsumer.testPost();
-//
-//        StepVerifier.create(response)
-//                .expectNextMatches(objectResponse -> objectResponse.getState().equals("ok"))
-//                .verifyComplete();
-//    }
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockBackEnd = new MockWebServer();
+        mockBackEnd.start();
+        var webClient = WebClient.builder().baseUrl(mockBackEnd.url("/").toString()).build();
+        restConsumer = new UserRestConsumer(webClient, userConsumerMapper);
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+
+        mockBackEnd.shutdown();
+    }
+
+    @Test
+    @DisplayName("Validate the function testGet.")
+    void validateTestGet() {
+
+        when( userConsumerMapper.toUserConsumer(any()) ).thenReturn(any());
+
+        mockBackEnd.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody("{\"state\" : \"ok\"}"));
+        var response = restConsumer.getUserByDocument("1");
+
+        StepVerifier.create(response)
+                .expectNextMatches(objectResponse -> !objectResponse.getDocument().isBlank())
+                .verifyComplete();
+    }
+
 }
