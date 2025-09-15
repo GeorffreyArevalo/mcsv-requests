bd8900f22bc3aec454216e385cd9b831bd61548b
Georffrey <georffreyarevalo@gmail.com>
2025-09-15 09:42:59 -0500

    Actuator health enabled

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/config/SecurityHeadersConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/config/SecurityHeadersConfig.java
@@ -32,7 +32,7 @@ public class SecurityHeadersConfig implements WebFilter {
 
         return Mono.just( request.getPath().value() )
                 .log()
-            .filter( path -> !path.contains("/openapi/") )
+            .filter( path -> !path.contains("/openapi/") && !path.equals("/actuator/health") )
             .flatMap( path ->
                 Mono.justOrEmpty( request.getHeaders().getFirst( HttpHeaders.AUTHORIZATION ) )
                 .switchIfEmpty( Mono.error( new CrediyaUnathorizedException(ExceptionMessages.UNAUTHORIZED_SENT_TOKEN_INVALID.getMessage())) )
